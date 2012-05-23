d0584c49780d788e5b854b0b106e29e4414b1469
James Bosko <boskojam@msu.edu>
2012-05-22 21:51:10 -0400

    Removed debugging outputs. @JJB

--- source/main/cDeme.cc
+++ source/main/cDeme.cc
@@ -1327,7 +1327,6 @@ void cDeme::SendInputsMessage(cAvidaContext& ctx)
       msg.SetTransCellID(m_input_cells[i]);
       if (cell.GetNumAVInputs()) {
         DoDemeInput(input_value);
-        cout << input_value << " -> " << m_input_cells[i] << endl;
       }
       for (int i = 0; i < cell.GetNumAVInputs(); i++) {
         cell.GetCellInputAVs()[i]->ReceiveMessage(msg);
