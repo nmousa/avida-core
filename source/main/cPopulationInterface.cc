deb91c0e3e8909b676f44c1f3dc958832992872f
Aaron P Wagner <apwagner@msu.edu>
2013-04-11 21:29:31 -0400

    Setting inject prey clone on pred attack to only clone viable prey.

--- source/main/cPopulationInterface.cc
+++ source/main/cPopulationInterface.cc
@@ -1645,7 +1645,24 @@ void cPopulationInterface::InjectPreyClone(cAvidaContext& ctx, int bg_id)
   while (org_to_clone == NULL) {
     cOrganism* org_at = TriedIdx[idx];
     // exclude pred and juvs & the genotype of the org that was being killed and triggered this cloning
-    if (org_at->GetForageTarget() > -1 && org_at->GetBioGroup("genotype")->GetID() != bg_id) org_to_clone = org_at;
+    if (org_at->GetForageTarget() > -1 && org_at->GetBioGroup("genotype")->GetID() != bg_id) {
+      cCPUTestInfo test_info;
+      cTestCPU* testcpu = m_world->GetHardwareManager().CreateTestCPU(ctx);
+      
+      int cell_id = org_at->GetCellID();
+      if (m_world->GetConfig().USE_AVATARS.Get()) cell_id = org_at->GetAVCellID();
+//      test_info.UseManualInputs(m_world->GetPopulation().GetCell(cell_id).GetInputs()); // Test using what the environment would be
+      
+      testcpu->TestGenome(ctx, test_info, Genome(org_at->GetGenome()));
+      if (test_info.GetTestPhenotype().GetGestationTime()) {
+        delete testcpu;
+        org_to_clone = org_at;   // only clone orgs that can reproduce on their own
+      }
+      else {
+        TriedIdx.Swap(idx, --list_size);
+        delete testcpu;
+      }
+    }
     else TriedIdx.Swap(idx, --list_size);
     if (list_size == 1) break;
     idx = m_world->GetRandom().GetUInt(list_size);
